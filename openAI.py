79dbb200cc6db75a59781e26dcd8026871deb423
Matatov <30751161+Matatov@users.noreply.github.com>
2023-05-27 15:58:44 +0300

    api key deleted

--- openAI.py
+++ openAI.py
@@ -5,7 +5,7 @@ import json
 
 url = 'https://api.openai.com/v1/chat/completions'
 
-api_key = "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"
+api_key = ""
 token = "Bearer " + api_key
 
 header = {"Content-Type": "application/json",
